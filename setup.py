0ccf7c5ab83bcf7b4cf9d94418b10bbbd17c63a3
John Woods <john.woods@intuitivemachines.com>
2017-10-31 17:11:09 +0000

    Prepared for pypi release (so you can now do pip install pyquat).

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+""" Python C quaternion type for fast attitude and rotation computations.
+See:
+https://github.com/mohawkjohn/pyquat
+"""
 
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
@@ -18,12 +22,43 @@ c_quat = Extension('pyquat/_pyquat',
                                     ('TINY_VERSION',  TINY)])
 setup(name='pyquat',
       version=version,
-      description='Python C quaternion type with attitude utility functions',
+      description='Python C quaternion type for fast attitude and rotation computations',
       author='John O. Woods, Ph.D.',
       author_email='john.woods@intuitivemachines.com',
-      url='http://www.intuitivemachines.com',
+      url='http://github.com/mohawkjohn/pyquat/',
+      download_url='https://github.com/mohawkjohn/pyquat/archive/v' + version + '.tar.gz',
       include_package_data=True,
-      packages=['pyquat', 'test'],
+      packages=['pyquat'],
       ext_modules=[c_quat],
-      test_suite='test.pyquat_test_suite'
+      test_suite='test.pyquat_test_suite',
+      license='BSD 3-clause',
+      classifiers=[ # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Software Development :: Embedded Systems',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Scientific/Engineering :: Physics',
+          'Topic :: Scientific/Engineering :: Visualization',
+
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: BSD License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+
+          'Programming Language :: C',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: Implementation :: CPython'
+      ],
+      keywords=['quaternion', 'math', 'maths', 'graphics', 'physics', 'orientation', 'attitude', 'pose', 'geometry', 'visualization', 'visualisation', 'animation', 'game development', 'simulation'],
+      install_requires=['numpy']
     )
