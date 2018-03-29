In rare cases, precision is lower than 14 decimal places. Fixes occasional Travis failure.

test/test_wahba_valenti.py
@@ -17,11 +17,11 @@ class TestWahbaValenti(QuaternionTest):
         
             q  = pqv.q_acc(a0)
             a1 = q.rotate(d)
-            np.testing.assert_almost_equal(a0, a1, decimal=14)
+            np.testing.assert_almost_equal(a0, a1, decimal=13)
 
             q  = pqv.dq_acc(a0)
             a1 = q.rotate(d)
-            np.testing.assert_almost_equal(a0, a1, decimal=14)
+            np.testing.assert_almost_equal(a0, a1, decimal=13)
 
     def test_q_mag_points_north(self):
         """ Tests that q_mag produces an arbitrary quaternion which points a random vector at [1,0,0]' (magnetic north); also tests that q_global_to_local gives the same result"""